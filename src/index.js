838a25ae43544600f783fb2f7dab51b12d6be6e8
Kacper Wąsik <iamkacperwasik@gmail.com>
2021-02-08 12:29:19 +0100

    fix(#23): change path.join to pathToFileURL(path.resolve)
    #23

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const {
   Menu,
 } = require("electron");
 const { nanoid } = require("nanoid");
+const { pathToFileURL } = require("url");
 
 const Entry = require("./types/entry");
 const {
@@ -222,7 +223,7 @@ const createWindow = () => {
   try {
     mainWindow = new BrowserWindow(WINDOW_OPTIONS);
 
-    mainWindow.loadFile(path.join(__dirname, "index.html"));
+    mainWindow.loadURL(pathToFileURL(path.resolve("src", "index.html")).href);
     mainWindow.maximize();
     mainWindow.on("close", handleExit);
     mainWindow.on("minimize", handleMinimize);
@@ -252,7 +253,9 @@ const createWindow = () => {
             parent: mainWindow,
           });
 
-          aboutWindow.loadURL(path.join(__dirname, "about.html"));
+          aboutWindow.loadURL(
+            pathToFileURL(path.resolve("src", "about.html")).href
+          );
           aboutWindow.removeMenu();
           aboutWindow.webContents.on("will-navigate", preventNavigation);
           aboutWindow.webContents.on("new-window", aboutLinkHandler);
